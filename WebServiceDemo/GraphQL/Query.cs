63edd81aa1723ca94d457efea2d9ab8b42f8493d
Jurgis Kargaudas <jurgis.kargaudas@railsbank.com>
2020-12-10 19:27:14 +0200

    update packages, expose teams on graphql

--- WebServiceDemo/GraphQL/Query.cs
+++ WebServiceDemo/GraphQL/Query.cs
@@ -11,5 +11,6 @@ namespace WebServiceDemo.GraphQL
     public class Query
     {
         public IEnumerable<Player> GetPlayers([Service] IPlayerRepository playerRepo) => playerRepo.GetPlayers();
+        public IEnumerable<Team> GetTeams([Service] ITeamRepository teamRepo) => teamRepo.GetTeams();
     }
 }
