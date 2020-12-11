5fa691bb7a87561a0d69a5b07eff5fb752260e7e
Jurgis Kargaudas <jurgis.kargaudas@railsbank.com>
2020-12-11 07:57:34 +0200

    more async examples

---
+++ WebServiceDemo/Helpers/AsyncService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebServiceDemo.Helpers
+{
+    public class AsyncService
+    {
+        public async Task<string> GetCalculation1()
+        {
+            var result = DoHardCalculationAsync().Result;
+            DoWorkNotDependantOnResult();
+
+            return result;
+        }
+
+        public async Task<string> GetCalculation2()
+        {
+            var result = await DoHardCalculationAsync();
+            DoWorkNotDependantOnResult();
+
+            return result;
+        }
+
+
+        private async Task<string> DoHardCalculationAsync()
+        {
+            await Task.Delay(2000);
+            return "A delicious sandwich";
+        }
+
+        private void DoWorkNotDependantOnResult()
+        {
+            Console.WriteLine("Off to make somea tea while result is fetched, brb");
+        }
+    }
+
+}
